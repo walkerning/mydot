b096c3391275d19b306c5686a7b25529029e88d4
walkerning <foxdoraame@gmail.com>
2016-01-07 20:00:50 +0800

    add ssh authorizedkeys

--- .dotfiles.py
+++ .dotfiles.py
@@ -38,9 +38,19 @@ def copy(src, dst):
     shutil.copy(src, dst)
 
 
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
 METHODS = {
     'append': append,
-    'copy': copy
+    'copy': copy,
+    'copytree': copytree
 }
 
 
