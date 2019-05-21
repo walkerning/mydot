d1189fe763d619f45100572c4499fae3275b7d70
Xuefei Ning <foxdoraame@gmail.com>
2019-05-21 14:58:17 +0800

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,20 @@
 ;; Not a fan of backup files
 (setq make-backup-files nil)
 
+;; Permit narrow-to-region
+(put 'narrow-to-region 'disabled nil)
+
+;; Setup package.el
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 ;; Configure flymake for Python
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
@@ -44,3 +58,18 @@
 (setq org-todo-keywords
       '((sequence "TODO(t)" "DOING(i)" "RUNNING(r)" "WAITING(w)" "HOLD(h)" "|" "DONE(d)" "ABANDONED(a)")))
 
+(org-defkey org-mode-map (kbd "M-C") 'org-table-insert-column)
+(org-defkey org-mode-map (kbd "M-R") 'org-table-insert-row)
+(org-defkey org-mode-map (kbd "C-M-K") 'org-table-delete-column)
+(org-defkey org-mode-map (kbd "C-c |") 'org-table-convert-region)
+(org-defkey org-mode-map (kbd "C-M-n") 'org-table-copy-down)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t) (python . t) (latex . t)))
+
+(setq org-src-fontify-natively t)
+
+(setq org-agenda-files (quote ("~/.emacs.d/agenda.org")))
+
+(setq org-startup-indented t) ; Enable `org-indent-mode' by default
+(add-hook 'org-mode-hook #'visual-line-mode)
